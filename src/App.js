03d4adfb6a664392c073e1114115ff9ddd87b29b
ruslankhh <ruslankhh@yandex-team.ru>
2018-11-16 02:48:43 +0300

    Добавить первую версию вёрстки

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BaseStyles } from '@primer/components';
+import { Header } from './components/Header/Header';
+import { Main } from './components/Main/Main';
+
+import "primer/index.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BaseStyles className="App">
+        <Header/>
+        <Main />
+      </BaseStyles>
     );
   }
 }
